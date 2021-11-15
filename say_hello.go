45d8a2de895a5ba628bbf42ce23fccb12ddd59a5
robbybenta <robbybenta7@gmail.com>
2021-11-15 21:38:19 +0700

    major changed

--- say_hello.go
+++ say_hello.go
@@ -4,6 +4,9 @@ package go_say_hello
 //git tag untuk rilis versi module
 // git tag v1.1.0
 // git push origin v1.1.0
-func SayHello() string  {
-	return "Hello brad"
+
+//jika terjadi major changed, maka ubah lah nama module, biasanya dikasih /v2
+// dan tag version diubdah drastis misalnya dari 1 menjadi 2
+func SayHello(name string) string  {
+	return "Hello brad" + name
 }
